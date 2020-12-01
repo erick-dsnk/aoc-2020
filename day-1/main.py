1d310623393991416bff3f9a83162755ec2c646c
erick <erick.8bld@gmail.com>
2020-12-01 15:07:43 +0200

    optimized day 1 part 2 algorithm

--- day-1/main.py
+++ day-1/main.py
@@ -1,4 +1,4 @@
-
+from timeit import Timer
 
 def get_input(filename: str):
     print(f"[*] Opening '{filename}'...")
@@ -21,11 +21,24 @@ def check_iteration_1(data, index):
     
     check_iteration_1(data, index + 1)
 
-def check_iteration_2(data): # a bit of a brute force solution but couldn't think of a solution similar to part 1
-    for i in data:
-        for j in data:
-            for k in data:
-                if i + j + k == 2020:
-                    print(f'[!] Found numbers that add up to 2020: `{i}` `{j}` `{k}`')
+def check_iteration_2(data, index):
+    print(f'[*] Checking iteration starting from index: i = {index}')
 
-                    return i, j, k
+    for i in data[index:(len(data) - 1)]:
+        for j in data[(index + 1):(len(data) - 1)]:
+            if i + j + data[index] == 2020:
+                print(f'[!] Found numbers that add up to 2020: `{i}` `{j}` `{data[index]}`')
+
+                return i, j, data[index]
+
+    check_iteration_2(data, index + 1)
+
+
+if __name__ == "__main__":
+    data = get_input('input.txt')
+
+    start = Timer()
+    pair_of_3 = check_iteration_2(data, 0)
+    end = Timer()
+
+    print(end - start)
